3649b94413cd614d6fead63c976ae1aa6d931d39
leesugyung <74905621+leesugyung@users.noreply.github.com>
2021-12-04 00:26:55 +0900

    front

---
+++ RoadEye/app/src/main/java/com/example/roadeye/VideoFragment.java
@@ -0,0 +1,22 @@
+package com.example.roadeye;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class VideoFragment extends Fragment {
+    private View view;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.fragment_video, container, false);
+
+        return view;
+    }
+}
