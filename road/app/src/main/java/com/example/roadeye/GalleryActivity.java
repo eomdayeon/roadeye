d995e58098c6fc5840ef1a9fb241dd8c61f93154
eomdayeon <69031678+eomdayeon@users.noreply.github.com>
2021-12-06 08:33:05 +0900

    전체 ui, mypage

---
+++ road/app/src/main/java/com/example/roadeye/GalleryActivity.java
@@ -0,0 +1,74 @@
+package com.example.roadeye;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.MediaController;
+import android.widget.VideoView;
+
+public class GalleryActivity extends AppCompatActivity {
+
+    private VideoView videoView; //비디오를 실행할 수 있게 도와주는 뷰
+    Button button;
+    Button btn_give;
+    private  static final  int REQUEST_CODE = 0;
+    private MediaController mediaController;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gallery);
+
+        button = findViewById(R.id.button);
+        btn_give = findViewById(R.id.btn_give);
+        videoView = findViewById(R.id.videoView); //비디오 뷰 아이디 연결
+
+        button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                intent.setType("video/*");
+                //Intent intent = new Intent();
+                //intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(intent, REQUEST_CODE);
+            }
+        });
+        btn_give.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(GalleryActivity.this, MapActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == REQUEST_CODE){
+            if(resultCode==RESULT_OK){
+                try{
+                    Uri uri = data.getData();
+                    //Glide.with(getApplicationContext()).load(uri).into(imageView);
+                    mediaController=new MediaController(this);
+                    mediaController.setAnchorView(videoView);
+                    videoView.setMediaController(mediaController);
+                    videoView.setVideoURI(uri); //비디오 뷰의 주소를 설정
+                    videoView.start(); //비디오 실행
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }else if(resultCode == RESULT_CANCELED){
+            }
+        }
+    }
+}
---
+++ road/app/src/main/java/com/example/roadeye/MainMapFragment.java
@@ -0,0 +1,120 @@
+package com.example.roadeye;
+
+import android.app.Dialog;
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.KeyEvent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBar;
+import androidx.fragment.app.Fragment;
+
+public class MainMapFragment extends Fragment {
+    private View view;
+    private WebView webView;
+    private String url = "http://car.daegu.go.kr/traffic/trafficInfo";
+
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.fragment_main_map, container, false);
+        ActionBar actionBar = ((MapActivity)getActivity()).getSupportActionBar();
+        actionBar.hide();
+        Button popup = (Button) view.findViewById(R.id.btn_popup);
+        webView = view.findViewById(R.id.webView);
+
+        popup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                open_popup(Gravity.CENTER);
+            }
+        });
+
+        webView = view.findViewById(R.id.webView);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.loadUrl(url);
+        webView.setWebChromeClient(new WebChromeClient());
+        webView.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                if(event.getAction()==KeyEvent.ACTION_DOWN){
+                    if(keyCode==KeyEvent.KEYCODE_BACK){
+                        if(webView!=null){
+                            if(webView.canGoBack()){
+                                webView.goBack();
+                            }else{
+                                getActivity().onBackPressed();
+                            }
+                        }
+                    }
+                }return true;
+            }
+        });
+
+
+
+
+        return view;
+    }
+
+    private class WebViewClientClass extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
+            return true;
+        }
+    }
+
+
+    private void open_popup(int gravity){
+        final Dialog dialog = new Dialog(getActivity());
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialog.setContentView(R.layout.dialog_popup);
+
+        Window window = dialog.getWindow();
+        if(window == null){
+            return;
+        }
+        window.setLayout(WindowManager.LayoutParams.MATCH_PARENT,WindowManager.LayoutParams.WRAP_CONTENT);
+        window.setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+
+        WindowManager.LayoutParams windowAttributes = window.getAttributes();
+        windowAttributes.gravity = gravity;
+        window.setAttributes(windowAttributes);
+
+        dialog.setCancelable(false);
+        Button btnno = dialog.findViewById(R.id.btn_no);
+        Button btnyes = dialog.findViewById(R.id.btn_yes);
+
+        btnno.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+            }
+        });
+
+        btnyes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //영상 보내기
+                Intent intent = new Intent(getActivity(),GalleryActivity.class);
+                startActivity(intent);
+            }
+        });
+        dialog.show();
+    }
+}
