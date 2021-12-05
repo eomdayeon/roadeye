de3f9b71d743b372710498948ae85d832ef8ab2a
leesugyung <74905621+leesugyung@users.noreply.github.com>
2021-12-06 03:29:08 +0900

    phase2

--- RoadEye/app/src/main/java/com/example/roadeye/VideoFragment.java
+++ RoadEye/app/src/main/java/com/example/roadeye/VideoFragment.java
@@ -1,22 +1,127 @@
 package com.example.roadeye;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
-public class VideoFragment extends Fragment {
-    private View view;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+
+public class VideoFragment extends Fragment  implements OnMapReadyCallback {
+    private View layout;
+    private MapView mapView = null;
+    private Button btn_request;
+    Thread thread;
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        view = inflater.inflate(R.layout.fragment_video, container, false);
+        layout = inflater.inflate(R.layout.fragment_video, container, false);
 
-        return view;
+        mapView = (MapView)layout.findViewById(R.id.map);
+        mapView.getMapAsync(this);
+
+        btn_request=(Button)layout.findViewById(R.id.btn_request);
+        btn_request.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                thread = new Thread(() -> {
+                    try {
+                        Thread.sleep(5000);
+                        Intent intent = new Intent(getActivity(),ReceiveActivity.class);
+                        startActivity(intent);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                });
+                thread.start();
+            }
+        });
+
+
+        return layout;
+    }
+
+    public void onStart(){
+        super.onStart();
+        mapView.onStart();
+    }
+
+    public void onStop(){
+        super.onStop();
+        mapView.onStop();
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        mapView.onSaveInstanceState(outState);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mapView.onLowMemory();
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+    //액티비티가 처음 생성될 때 실행되는 함수
+
+        if(mapView != null)
+        {
+            mapView.onCreate(savedInstanceState);
+        }
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        LatLng location = new LatLng(35.892486, 128.609185); //경북대학교 북문
+        MarkerOptions markerOptions = new MarkerOptions();
+        markerOptions.title("경북대학교 북문 교차로");
+        markerOptions.snippet("35.892486, 128.609185");
+        markerOptions.position(location);
+        googleMap.addMarker(markerOptions);
+
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(location, 16));
+
     }
 }
